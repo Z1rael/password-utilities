e5edbc403013eeeb20fc2efdd73cad1633a84b70
Felix Dilly <felix.dilly@gmail.com>
2026-04-15 15:18:17 +0200

    feat: added tests for isPasswordString and generatePassword

---
+++ test/index.test.js
@@ -0,0 +1,39 @@
+const {generateStrongPassword, isStrongPassword} = require('../src/index')
+
+describe('isStrongPassword', () => {
+    test('check if a too short password is flagged as not strong', () => {
+        expect(isStrongPassword('pC%mD8T')).toEqual(false);
+    })
+
+    test('check if a missing special character is flagged as not strong', () => {
+        expect(isStrongPassword('Hello World')).toEqual(false);
+    })
+
+    test('check if a missing lower case character is flagged as not strong', () => {
+        expect(isStrongPassword('PC%MD8TPCKN2')).toEqual(false); 
+    })
+
+    test('check if a missing higher case character is flagged as not strong', () => {
+        expect(isStrongPassword('pc%md8tpckn2')).toEqual(false); 
+    })
+
+    test('check if a missing number character is flagged as not strong', () => {
+        expect(isStrongPassword('pc%mdtpckn')).toEqual(false); 
+    })
+
+
+})
+
+describe('generateStrongPassword', () => {
+    test('generate a length 1 password', () => {
+        expect( generateStrongPassword(1).length == 1)
+    })
+
+    test('check if a generated password is strong', () => {
+        expect(isStrongPassword(generateStrongPassword(8)));
+    })
+
+    test('check if a generated password is not strong', () => {
+        expect(isStrongPassword(generateStrongPassword(7)));
+    })
+})
